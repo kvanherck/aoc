19b05a019e684afb68686d5bdbb144a2de397443
Koen Van Herck <koen.v.herck@gmail.com>
2023-12-09 16:31:54 +0100

    AOC 2023 day 9

---
+++ 2023/09/p1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append('..')
+from common import *
+import numpy as np
+
+score = 0
+for line in inputLines():
+    a = np.array(list(map(int, line.split())), dtype=int)
+    trace = []
+    while not np.all(a == 0):
+        trace.append(a[-1])
+        a = a[1:] - a[:-1]
+    score += np.sum(trace)
+
+print(score)
